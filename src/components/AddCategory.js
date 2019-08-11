f6d8c007044636784c2aa10158de580efb767cfc
Rohan Mahajan <rohan.mahajan@gartner.com>
2019-08-11 16:03:49 +0530

    add category and container components added

---
+++ src/components/AddCategory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCategory = () => {
+    return(
+        <div className="add-category">
+            hi add categories here
+        </div>
+    );
+}
+
+export default AddCategory; 
--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import '../styles/main.scss';
 import Header from './Header';
 import Footer from './Footer';
+import Container from './Container';
+import AddCategory from './AddCategory';
 
 const App = () => {
 
     return (
         <>
             <Header/>
+            <Container>
             <Router>
                 <Switch>
-                    {/* <Route exact path="/" component={Home}/> */}
-                    <h1>Expensify</h1>
+                    <Route exact path="/" component={AddCategory}/>
                 </Switch>
             </Router>
+            </Container>
+            
             <Footer/>
         </>
     );
