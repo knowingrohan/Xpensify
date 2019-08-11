8e7f475bc10c628e6ed94a5bf8d32c0b169a0652
Rohan Mahajan <rohan.mahajan@gartner.com>
2019-08-11 14:37:44 +0530

    development in progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { SideNav, SideNavItem, Button, Icon } from 'react-materialize';
 import Logo from '../assets/logo.png';
 const Header = () => {
     return (
         <header className="header">
             <div className="header__logo">
-                <img src={Logo}  alt = "Site's logo" />
+                <img src={Logo} alt="Site's logo" />
             </div>
-           
+            <div>
+                <SideNav trigger={
+                    <Button node="a" waves="light" small>
+                        <Icon>account_balance</Icon>
+                    </Button>}
+                    options={{ 
+                        closeOnClick: true,
+                        edge: 'right',
+                        draggable : true
+                    }}
+                >
+
+                    <SideNavItem href="#!icon" icon="cloud">
+                        First Link With Icon
+                    </SideNavItem>
+                    <SideNavItem href="#!second">
+                        Second Link
+                    </SideNavItem>
+                    <SideNavItem divider />
+                    <SideNavItem subheader>
+                        Subheader
+                    </SideNavItem>
+                    <SideNavItem waves href="#!third">
+                        Third Link With Waves
+                    </SideNavItem>
+                </SideNav>
+            </div>
         </header>
     );
 }
